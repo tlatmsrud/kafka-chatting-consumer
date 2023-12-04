99e3a3605fb26dc99c4b395d13d247d36ac74b76
seungkeungsim <tlatmsrud@naver.com>
2023-12-04 18:22:40 +0900

    :sparkles: Add-ChattingRecord-for-kafka-chat-application

---
+++ src/main/java/org/ssk/domain/chatting/record/ChattingRecord.java
@@ -0,0 +1,26 @@
+package org.ssk.domain.chatting.record;
+
+import lombok.*;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@Setter
+public class ChattingRecord {
+
+    private String nickname;
+    private String message;
+    private String time;
+    private Long roomId;
+
+    private ChattingRecord(String nickname, String message, String time, Long roomId){
+        this.nickname = nickname;
+        this.message = message;
+        this.time = time;
+        this.roomId = roomId;
+    }
+
+    public ChattingRecord of(String nickname, String message, String time, Long roomId){
+        return new ChattingRecord(nickname, message, time, roomId);
+    }
+}
